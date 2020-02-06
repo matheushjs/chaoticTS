369a2b27ed8ed45170ac7b71df974c23151eb006
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-02-05 21:31:59 -0300

    Organize code

--- electricity-dwnn/prog.r
+++ electricity-dwnn/prog.r
@@ -1,15 +1,5 @@
 require(tseriesChaos);
 
-df = read.csv("../sunspot.csv", header=F, sep=";");
-colnames(df) = c("year", "month", "numdate", "mean", "sd", "samples", "def-or-prov");
-
-m = 8;
-d = 8;
-
-# x1 x2 x3
-# x2 x3 x4 ...
-emb = embedd(df$mean, m=m, d=d);
-
 # Receives the dataset, which is a matrix with rows (x1, x2, x3, y)
 # And receives the query point (x1, x2, x3)
 dwnn = function(dataset, query, sigma=0.5){
@@ -32,9 +22,20 @@ dwnn = function(dataset, query, sigma=0.5){
 report = function(correct, obtained){
 	squares = (correct - obtained)**2
 	naCount = sum(is.na(squares));
-	mse = mean(squares[!is.na(squares)]);
-	cat("MSE: ", mse, "\tNA samples: ", naCount, "\n");
+	rmse = sqrt(mean(squares[!is.na(squares)]));
+	cat("RMSE: ", rmse, "\tNA samples: ", naCount, "\n");
 }
+
+
+df = read.csv("../sunspot.csv", header=F, sep=";");
+colnames(df) = c("year", "month", "numdate", "mean", "sd", "samples", "def-or-prov");
+
+m = 4;
+d = 34;
+
+# x1 x2 x3
+# x2 x3 x4 ...
+emb = embedd(df$mean, m=m, d=d);
 
 beginTest = floor(nrow(emb) * 0.7);
 train = emb[1:(beginTest-1),];
