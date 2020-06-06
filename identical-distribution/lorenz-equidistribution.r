e96cbe1ad4c7bf29b28d32e63bf6f176d812370b
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-06-06 01:43:00 -0300

    Add lorenz-equidistribution.r

---
+++ identical-distribution/lorenz-equidistribution.r
@@ -0,0 +1,21 @@
+require(tseriesChaos);
+
+#lorenz.ts <- sim.cont(lorenz.syst, 0, 40000, 0.05,
+#     start.x=c(5,5,5), parms=c(10, 28, -8/3), obs.fun = function(x) x[1]);
+
+lorenz.ts = read.table("lor.dat");
+
+states = embedd(lorenz.ts, m=2, d=4);
+
+plotIdx = function(idx){
+	maxLength = nrow(states);
+
+	all = NULL;
+	for(i in 1:4000){
+		rand = round(runif(n=1, min=1, max=maxLength-idx));
+		state = states[rand+idx,];
+		all = rbind(all, state);
+	}
+
+	plot(all, pch=19, col="#00000033");
+}
