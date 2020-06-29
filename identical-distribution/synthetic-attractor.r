12bf24c4614d0ea94844f2d6369731e97c6f3139
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-06-29 19:44:00 -0300

    Make it possible to animate the phase space

--- identical-distribution/synthetic-attractor.r
+++ identical-distribution/synthetic-attractor.r
@@ -6,7 +6,7 @@ require(tseriesChaos);
 
 # This difeomorphism applies to y
 D = function(y){
-	1 / (1 + exp(-6*pi*y))
+	1 / (1 + exp(-pi*y))
 }
 
 # This one applies to x
@@ -18,13 +18,31 @@ Dx = function(x){
 	int*2*pi + frac**( 1 - ((frac - 0)*(2*pi - frac) / peak / 2) );
 }
 
-
 #x = seq(0, 8*pi, length=10000)
 #y = sin(Dx(x))
 #plot(x, y);
 #plot(embedd(y, m=2, d=140), pch=19, col="#00000011")
 
-x = seq(0, 20, length=9999);
-y = D(sin(2*pi*x));
-plot(embedd(y, m=2, d=10)[1:1000,], pch=19, col="#00000033")
+series = function(n = 100){
+	t0 = runif(n=1, max=1000, min=0);
+	t = t0 + 1:n * 40/9999;
+	D(sin(2*pi*t));
+}
 
+#x = seq(0, 20, length=9999);
+#y = D(sin(2*pi*x));
+#plot(embedd(y, m=2, d=10)[1:1000,], pch=19, col="#00000033")
+
+plot(embedd(series(1000), m=2, d=10), pch=19, col="#00000011");
+
+all = list()
+for(i in 1:10000){
+	all[[i]] = embedd(series(60), m=2, d=10);
+}
+
+plotAll = function(idx=1){
+	mat = matrix(ncol=2, nrow=length(all));
+	for(i in 1:length(all))
+		mat[i,] = all[[i]][idx,];
+	plot(mat, pch=19, col="#00000004");
+}
