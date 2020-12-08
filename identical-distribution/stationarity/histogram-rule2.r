5588c5c4629992634de73d96d3d74700e9fe7023
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-12-08 04:05:01 -0300

    Make plots

--- identical-distribution/stationarity/histogram-rule2.r
+++ identical-distribution/stationarity/histogram-rule2.r
@@ -152,4 +152,113 @@ lorenz.single = function(data){
 	result;
 }
 
+simulate = function(data, divisions=6, name="Lorenz", plot.average=F, newPlot=T, plotIdx=1){
+	N = nrow(data);
+	m = 3;
+	d = 3;
 
+	xrange = range(data[,1]);
+	yrange = range(data[,2]);
+	zrange = range(data[,3]);
+
+	if(newPlot)
+		plot(0, 1, xlab="sample size", ylab="Divergence", type="l", xlim=c(0, N), ylim=c(1e-2, 1.3), #log="y",
+			 main=paste("Full histogram divergences (", name,")", sep=""), col="#FFFFFF");
+
+	avgResult = NULL;
+	for(i in seq(0, divisions-1))
+	for(j in seq(0, divisions-1))
+	for(k in seq(0, divisions-1)){
+		#if( any(c(i, j, k) != c(0, 2, 0)) ) next;
+
+		box.x = min(xrange) + c(i, i+1) * diff(xrange) / divisions;
+		box.y = min(yrange) + c(j, j+1) * diff(yrange) / divisions;
+		box.z = min(zrange) + c(k, k+1) * diff(zrange) / divisions;
+
+		cat(i, j, k, sep="/");
+		cat("\n");
+		print(rbind(box.x, box.y, box.z));
+
+		# if resampling
+		#data = data[seq(1, nrow(data), by=5),]
+
+		#rgl::plot3d(data);
+		#rgl::lines3d(t(rbind(box.x, box.y, box.z)), col=2, lwd=3);
+
+		inBox = (data[,1] > box.x[1]) & (data[,1] < box.x[2]);
+		inBox = inBox & (data[,2] > box.y[1]) & (data[,2] < box.y[2]);
+		inBox = inBox & (data[,3] > box.z[1]) & (data[,3] < box.z[2]);
+		
+		#print(inBox[seq(1, 10000, by=97)]);
+		freq = sum(inBox) / nrow(data);
+
+		#if(freq == 0)
+		print(N / (divisions**3));
+		print(sum(inBox));
+		if(sum(inBox) < N / (divisions**2))
+			next;
+
+		if(plot.average){
+			result = divergence.plot(data, box.x, box.y, box.z,
+								 to=N/2, by=N%/%1000, shuffle=F, plot=F);
+
+			if(is.null(avgResult)){
+				avgResult = result[,c(1, 4)];
+			} else {
+				avgResult[,2] = (avgResult[,2] + result[,4]);
+			}
+		} else {
+			result = divergence.plot(data, box.x, box.y, box.z,
+								 to=N/2, by=N%/%1000, shuffle=F);
+		}
+	}
+
+	if(plot.average){
+		# Times two because it holds the size of one half only
+		lines(2*avgResult[,1], avgResult[,2], lwd=2, col=plotIdx, lty=plotIdx);
+		grid(col="#00000044");
+	}
+
+	#epsilon = function(eta, x){
+	#	sqrt(
+	#		 2 * log(eta/2) / (-2*x)
+	#	) / 2;
+	#}
+
+	x = seq(0, N * 1.1, length=1000);
+	#lines(x, epsilon(0.05, x), col=2);
+	#lines(x, epsilon(0.001, x), col=3);
+	#lines(x, epsilon(1e-10, x), col=4);
+	#lines(x, epsilon(0.2, x), col=5);
+
+	#legend("topright", c(expression(eta == 0.05), expression(eta == 0.001), expression(eta == 1e-10), expression(eta == 0.2)), col=c(2, 3, 4, 5), lwd=1);
+	#legend("bottomleft", "the probability to exceed the\ncolored lines is at most eta");
+
+	if(plot.average){
+		return(avgResult);
+	} else {
+		return(result);
+	}
+}
+
+data = lorenz.real(5000); # 1,500,000 uses up about 60 MB
+data2 = logistic(5000); data2 = embedd(data2, m=3, d=1);
+result = simulate(data, name="Whatever", divisions=10, plot.average=T);
+result = simulate(data2, name="NOAA Vancouver Precipitation", divisions=10, plot.average=T, newPlot=F, plotIdx=2);
+
+#data = read.csv("../../sunspot.csv", sep=";"); data = data[,4]; data = embedd(data, m=3, d=17);
+
+data = read.csv("../../NOAA-vancouver-clean.csv"); data = data[,"TAVG"]; data = embedd.custom(data, c(1, 2, 365));
+result = simulate(data, name="NOAA Vancouver Precipitation", divisions=10, plot.average=T, newPlot=F, plotIdx=3);
+
+#data2 = read.csv("../../NOAA-vancouver-clean.csv"); data2 = data2[,"PRCP7"]; data2 = data2[!is.na(data)]; data2 = data2[seq(1, length(data), by=7)]; data2 = embedd.custom(data2, c(1, 2, 365%/%7));
+#data2 = read.csv("../../NOAA-vancouver-clean.csv"); data2 = data2[,"PRCP7"]; data2 = data2[!is.na(data)]; data2 = embedd.custom(data2, c(1, 2, 365));
+#data = read.csv("../../NOAA-vancouver-clean.csv"); data = data[,"PRCP7"]; data = data[!is.na(data)]; data = embedd.custom(data, c(1, 2, 365));
+#result = simulate(data, name="Whatever", divisions=10, plot.average=T);
+#result = simulate(data2, name="NOAA Vancouver Precipitation", divisions=10, plot.average=T, newPlot=F, plotIdx=2);
+
+#result = lorenz.single(data);
+
+legend("topright", c("Lorenz", "Logistic", "NOAA Temperatures"), col=1:3, lty=1:3, lwd=2);
+
+#savePlot("histogram-rule-lorenz.png");
